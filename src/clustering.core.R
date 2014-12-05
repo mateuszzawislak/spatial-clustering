5ebb7ed09bd903d654a1eb21d62ec9eb884170bc
mateuszzawislak <mateusz.andrzej.zawislak@gmail.com>
2014-12-05 14:54:35 +0100

    prowizoryczna struktura opisu zbioru danych

--- src/clustering.core.R
+++ src/clustering.core.R
@@ -9,6 +9,40 @@
 library(sp)
 
 
+# by default columns are treated as real
+example.data.description <- list(
+    "classIndex" = 1,
+    "points" = list(list("x" = 2, "y" = 3),list("x" = 4, "y" = 5)),
+    "geoPoints" = list(list("long" = 7, "lat" = 6))
+  )
+
+printDataDescription <- function(description) {
+  print('Class index')
+  print(description$classIndex)
+  
+  if(length(description$points) > 0 ) {
+    for(i in 1:length(description$points)) {
+      print('Point: ')
+      print(description$points[i])
+    }
+  }
+}
+
+# Utils
+get.column.index <- function(data, columnName) {
+  match(columnName, colnames(data))
+}
+
+get.column.name <- function(data, index) {
+  colnames(data)[index]
+}
+
+get.attributes.indexes <- function(columns.number, data.description) {
+  indexes <- 1:columns.number
+  indexes[-data.description$classIndex]
+}
+
+
 # distance metrics
 distance.bray.curtis <- function(val1, val2) {
   abs(val1 - val2) / (val1 + val2)
@@ -23,7 +57,7 @@ distance.real <- function(val1, val2) {
   difference
 }
 
-objects.distance <- function(d1, d2) {
+objects.distance <- function(d1, d2, data.description) {
   distance = 0
   for(i in 1:length(d1)) {
     # TODO
@@ -35,10 +69,10 @@ objects.distance <- function(d1, d2) {
 
 # returns the vector of distances from object to each one from objects
 # vector length is equal to objects count
-calculate.distances.from.object <- function(objects, object) {
+calculate.distances.from.object <- function(objects, object, data.description) {
   distances <- c()
   for(i in 1:nrow(objects)) {
-    dist <- objects.distance(objects[i,], object)
+    dist <- objects.distance(objects[i,], object, data.description)
     distances <- append(distances, dist)
   }
   
@@ -47,29 +81,30 @@ calculate.distances.from.object <- function(objects, object) {
 
 # calculates objects distance matrix
 # calculates only bottom-left part of matrix, because it is symetric
-calculate.distance.matrix <- function(objects) {
+calculate.distance.matrix <- function(objects, data.description) {
   require(geosphere)
   
-  distances.from.object <- function(objectIndex, objects) {
+  distances.from.object <- function(objectIndex, objects, data.description) {
     distances <- rep(0,nrow(objects))
 
+    attributes.indexes <- get.attributes.indexes(ncol(objects), data.description)
+    
     # from the given object to the end of the row
-    distances[objectIndex:nrow(objects)] <- calculate.distances.from.object(objects[objectIndex:nrow(objects),2:ncol(objects)],objects[objectIndex,2:ncol(objects)])
+    distances[objectIndex:nrow(objects)] <- calculate.distances.from.object(objects[objectIndex:nrow(objects),attributes.indexes],objects[objectIndex,attributes.indexes],data.description)
     return(distances)
   }
   
   # for every object calculate distances to all objects
   # hint: cbind to pionowe z³¹cznie wektorów
-  dm <- do.call(cbind, lapply(1:nrow(objects), distances.from.object, objects))
+  dm <- do.call(cbind, lapply(1:nrow(objects), distances.from.object, objects, data.description))
   return(as.dist(dm))
 }
 
-
-main <- function() {
-  uciData <- read.csv(file="D:/Github/spatial-clustering/data/wine.data", head=FALSE, sep=",", skip=0)
-  
+spatial.cluster <- function(data, data.description) {
   # distance matrix
-  objects.distances.matrix <- calculate.distance.matrix(uciData)
-  km <- kmeans(objects.distances.matrix, centers=3)
-  print(km)
+  objects.distances.matrix <- calculate.distance.matrix(data, data.description)
+  
+  # cluster data
+  cluster.model <- kmeans(objects.distances.matrix, centers=3)
+  cluster.model
 }
--- src/clustering.quality.R
+++ src/clustering.quality.R
@@ -6,4 +6,21 @@
 # December 2014
 #
 
-source('clustering.core.r')
+setwd("D:/Github/spatial-clustering/src")
+
+source('clustering.core.R')
+
+wine.data.description <- list(
+  "classIndex" = 1
+)
+
+remove.class.column <- function(data, classIndex) {
+  data[,-classIndex]
+}
+
+main <- function() {
+  uciData <- read.csv(file="../data/wine.data", head=FALSE, sep=",", skip=0)
+  
+  cluster.model <- spatial.cluster(uciData, wine.data.description)
+  print(cluster.model)
+}
