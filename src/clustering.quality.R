01e1e4a21f02af55a621f9a442784b40066fa073
mateuszzawislak <mateusz.andrzej.zawislak@gmail.com>
2014-12-06 15:03:30 +0100

    generating plots

--- src/clustering.quality.R
+++ src/clustering.quality.R
@@ -8,22 +8,57 @@
 
 source('clustering.core.R')
 
+split.data <- function(data, percent) {
+  # calculate columns count
+  columns.count <- floor(nrow(data) * percent / 100)
+  # get data random indexes
+  random.indexes <- sample(seq(1,nrow(data)),columns.count, replace=FALSE)
+  
+  # select data
+  selected.data <- data[random.indexes,]
+  
+  selected.data
+}
+
 rate.cluster <- function(cluster.model, data, data.description) {
   class.column <- data.description$classIndex
   
   # TODO
+  sample(seq(0.1, 0.9, 0.1), 1)
 }
 
 remove.class.column <- function(data, classIndex) {
   data[,-classIndex]
 }
 
-test.clustering <- function(data, data.description) {
-  cluster.model <- spatial.cluster(data, data.description)
+test.clustering <- function(data, data.description, data.description.spatial) {
+  execution.times <- c()
+  execution.times.spatial <- c()
+  clustering.quality <- c()
+  clustering.quality.spatial <- c()
   
-  quality <- rate.cluster(cluster.model, data, data.description)
+  portions <- seq(10, 100, 10)
   
-  print(cluster.model)
-  print('Quality')
-  print(quality)
+  for(portion in portions) {
+    splitted.data <- split.data(data, portion)
+    
+    execution.time <- system.time(cluster.model <- spatial.cluster(splitted.data, data.description))
+    execution.time.spatial <- system.time(cluster.model.spatial <- spatial.cluster(splitted.data, data.description.spatial))
+    
+    quality <- rate.cluster(cluster.model, data, data.description)
+    quality.spatial <- rate.cluster(cluster.model.spatial, data, data.description)
+    
+    print('done')
+    execution.times = append(execution.times, execution.time[3])
+    execution.times.spatial = append(execution.times.spatial, execution.time.spatial[3])
+    clustering.quality = append(clustering.quality, quality)
+    clustering.quality.spatial = append(clustering.quality.spatial, quality.spatial)
+  }
+  
+  # plots
+  plot(portions, clustering.quality, type = "p", pch = 4, col = "blue", xlab = "percent of training data [%]", ylab = "clustering quality")
+  points(portions, clustering.quality.spatial, type = "p", pch = 8, col = "red")
+  
+  plot(portions, execution.times.spatial, type = "l", col = "red", xlab = "percent of training data [%]", ylab = "execution time [s]")
+  points(portions, execution.times, type = "l", col = "blue")
 }
--- src/clustering.tests.R
+++ src/clustering.tests.R
@@ -10,15 +10,20 @@ setwd("D:/Github/spatial-clustering/src")
 
 source('clustering.quality.R')
 
-wine.data.description <- list(
+wine.data.description.spatial <- list(
   "ignore.columns" = c("V9"),
   "string.columns" = c("V1"),
   "points" = list(list("x" = "V2", "y" = "V5")),
   "geographic.coordinates" = list(list("long" = "V3", "lat" = "V4"))
 )
 
+wine.data.description <- list(
+  "class.column" = "V1",
+  "ignore.columns" = c("V9")
+)
+
 main <- function() {
   uciData <- read.csv(file="../data/wine.data", head=FALSE, sep=",", skip=0, dec=".")
   
-  system.time(test.clustering(uciData, wine.data.description))
+  system.time(test.clustering(uciData, wine.data.description, wine.data.description.spatial))
 }
